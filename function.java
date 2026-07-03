913abd051e81fc8246c47facc0bbeca2e1d1c88a
hetshah29 <hetshah2906@gmail.com>
2026-07-03 09:50:01 +0530

    Initial commit

---
+++ function.java
@@ -0,0 +1,10 @@
+class function{
+    int sum(int a,int b){
+        int c=a+b;
+        return a+b;
+    }
+public static void main(String[] args) {
+    System.out.println("answer");
+    display sum()
+}
+}

5fcb6bd2e0c569815d8f0601a516c6a13c97d853
het shahgo <hetshah2906@gmail.com>
2026-07-09 08:46:52 +0530

    Update portfolio page

--- function.java
+++ function.java
@@ -1,12 +1,5 @@
 class function{
     public static void main(String[] args) {
-        // Declaration
-int[] arr;
-
-// Declaration + memory allocation
-int[] arr = new int[5];
-
-// Declaration + initialization
-int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("hello this is my first program");
     }
 }
